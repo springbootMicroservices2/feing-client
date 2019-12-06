a0e40e8ec93c030091e917b2dca1e247c318d2af
springbootMicroservices2 <57837154+springbootMicroservices2@users.noreply.github.com>
2019-12-06 22:45:14 +0530

    eureka server

---
+++ springboot-eureka-demo/src/test/java/com/example/springboot/eurekaserver/springbooteurekademo/SpringbootEurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.eurekaserver.springbooteurekademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
